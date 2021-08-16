d120fbb38fe472563ab4a21deaf3d9dc744f30cd
Manoj Panta <manojpanta@Manojs-MacBook-Pro.local>
2021-08-16 11:52:47 -0500

    add code

---
+++ src/main/java/SumOfNum.java
@@ -0,0 +1,9 @@
+public class SumOfNum {
+    public static int sum(int a, int b){
+        int sum = a+b;
+        return sum;
+    }
+    public static void main(String[] args){
+        System.out.println(sum(-4,5));
+    }
+}
